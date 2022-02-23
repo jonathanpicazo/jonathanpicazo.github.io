5d02207801dc589e1fe8a6f75d5c27d64fb07387
Jonathan Picazo <jonathanpicazo@outlook.com>
2022-02-23 11:07:27 -0800

    finished adding dark mode

--- js/custom/addon.js
+++ js/custom/addon.js
@@ -1,18 +1,33 @@
 // FOR DARK MODE
 
 let darkModeCheckbox = document.getElementById('theme-toggle')
-darkModeCheckbox.addEventListener('change', toggleTheme)
+darkModeCheckbox.addEventListener('click', toggleTheme)
 
 
 function toggleTheme(evt) {
-  console.log(evt)
-  if (evt.target.checked) {
-    console.log(document.body)
-    document.body.classList.remove('dark-mode')
+  let target = evt.currentTarget.firstChild
+  if (evt.target.classList[1] === 'fa-moon') {
+    document.body.classList.add('dark-mode')
+    target.classList.remove('fa-moon')
+    target.classList.add('fa-sun')
   }
   else {
-    
-    document.body.classList.add('dark-mode')
+    document.body.classList.remove('dark-mode')
+    target.classList.add('fa-moon')
+    target.classList.remove('fa-sun')
   }
 }
-// smooth scroll
+
+let btns = document.querySelectorAll(".scroll-to")
+console.log(btns)
+if (btns) {
+  btns.forEach(function(el){
+    el.addEventListener('click', scrollPop);
+  });
+}
+
+function scrollPop(evt) {
+  let target = evt.currentTarget
+  let associatedBtn = document.getElementById(target.id + '-btn')
+  associatedBtn.click()
+}
